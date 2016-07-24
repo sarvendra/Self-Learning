2a0e0a0f92470720038e78e8963cdec1aa21021f
sarvendra <sarvendraanand@gmail.com>
2016-07-24 15:38:21 +0530

    Use spring aop

---
+++ SpringInAction/src/main/java/com/learning/Minstrel.java
@@ -0,0 +1,11 @@
+package com.learning;
+
+public class Minstrel {
+    public void singBeforeQuest() {
+        System.out.println("Fa la la, knight is going for quest");
+    }
+
+    public void singAfterQuest() {
+        System.out.println("Te he he, knight is done with quest");
+    }
+}
