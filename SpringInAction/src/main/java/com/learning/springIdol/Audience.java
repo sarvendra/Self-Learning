70c0bec27af132fdefe8c35c4e0d75ec0b58b9e7
sarvendra <sarvendraanand@gmail.com>
2016-09-18 13:09:46 +0530

    basic annotation AOP

--- SpringInAction/src/main/java/com/learning/springIdol/Audience.java
+++ SpringInAction/src/main/java/com/learning/springIdol/Audience.java
@@ -1,20 +1,29 @@
 package com.learning.springIdol;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 
+@Aspect
 public class Audience {
+    @Pointcut("execution(* com.learning.springIdol.Performer.perform(..))")
+    public void performance() {}
+
+    @Before("performance()")
     public void takeSeats() {
         System.out.println("The audience is taking their seats");
     }
 
+    @Before("performance()")
     public void turnOffCellPhones() {
         System.out.println("The audience is turning off their cellphones");
     }
 
+    @AfterReturning("performance()")
     public void applaud() {
         System.out.println("The audience is clapping CLAP CLAP CLAP CLAP");
     }
 
+    @AfterThrowing("performance()")
     public void demandRefund() {
         System.out.println("Boo! We want our money back");
     }
--- SpringInAction/src/main/java/com/learning/springIdol/PerformerMain.java
+++ SpringInAction/src/main/java/com/learning/springIdol/PerformerMain.java
@@ -7,7 +7,6 @@ public class PerformerMain {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-idol.xml");
         Performer performer = (Performer) context.getBean("kenny");
-        Contestant contestant = (Contestant) performer;
-        contestant.receiveAwards();
+        performer.perform();
     }
 }
